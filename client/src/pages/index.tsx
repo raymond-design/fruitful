52a90176f415741d5c427b351aaa91bdedcf2668
raymond-design <raymondyan777@gmail.com>
2022-02-02 12:42:30 -0500

    added SSR option for home page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -8,6 +8,7 @@ import Axios from 'axios';
 
 import styles from '../styles/Home.module.css'
 import { Post } from '../types'
+import { GetServerSideProps } from 'next'
 
 dayjs.extend(relativeTime);
 
@@ -18,8 +19,8 @@ export default function Home() {
     Axios.get('/posts')
     .then(res => setPosts(res.data))
     .catch(err => console.log(err))
-  }, [])
-
+  }, []) 
+  
   return (
     <div className="pt-12">
       <Head>
@@ -27,16 +28,15 @@ export default function Home() {
       </Head>
       <div className="container flex pt-4">
         <h1>
-          Your Feed
         </h1>
-        {/*Posts*/}
+        {/*Feed*/}
         
         <div className="w-160">
           {posts.map((post) => (
             <div key={post.identifier} className="flex mb-4 bg-white rounded">
               {/*votes*/}
               <div className="w-10 text-center bg-gray-200 rounded-l">
-                <p>YO</p>
+                <p>Title</p>
               </div>
               {/*info/data about post*/}
               <div className="w-full p-2">
@@ -45,7 +45,7 @@ export default function Home() {
                     <Fragment>
                       <img src="http://www.gravatar.com/avatar" className="w-6 h-6 mr-1 rounded-full cursor-pointer" />
                       <a className="font-bold text-cs hover:underline">
-                      /group/{post.group}
+                        Group: {post.group}
                       </a>
                     </Fragment>
                   </Link>
@@ -63,6 +63,24 @@ export default function Home() {
                 </div>
                 <Link href={post.url}><a className="my-1 text-lg font-medium">{post.title}</a></Link>
                 {post.body && <p className="my-1 text-sm">{post.body}</p>}
+                <div className="flex">
+                  <Link href={post.url}>
+                    <a>
+                      <div className="px-1 mr-2 text-xs text-gray-500 rounded cursor-pointer hover:bg-gray-200">
+                        <i className="mr-1 fas fa-comment-alt fa-xs"></i>
+                        <span className='font-bold'>20 Comments</span>
+                      </div>
+                    </a>
+                  </Link>
+                  <div className="px-1 mr-2 text-xs text-gray-500 rounded cursor-pointer hover:bg-gray-200">
+                    <i className="mr-1 fas fa-bookmark fa-xs"></i>
+                    <span className='font-bold'>Save</span>
+                  </div>
+                  <div className="px-1 mr-2 text-xs text-gray-500 rounded cursor-pointer hover:bg-gray-200">
+                    <i className="mr-1 fas fa-share fa-xs"></i>
+                    <span className='font-bold'>Share</span>
+                  </div>
+                </div>
               </div>
             </div>
           ))}
@@ -71,3 +89,13 @@ export default function Home() {
     </div>
   )
 }
+
+/* export const getServerSideProps: GetServerSideProps = async (context) => {
+  try {
+    const res = await Axios.get('/posts');
+
+    return { props: { posts: res.data } };
+  } catch (error) {
+    return { props: { error: 'Something went wrong!'} };
+  }
+} */
