4ed1a52bdbefdd2c9657f1343791fe0c21bd2b32
raymond-design <raymondyan777@gmail.com>
2022-03-03 13:48:36 -0500

    typeorm query builder to fetch expected array of group objects

--- src/routes/other.ts
+++ src/routes/other.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from 'express'
 
+import Group from '../entities/Group';
 import Post from '../entities/Post';
 import User from '../entities/User'
 import Vote from '../entities/Vote';
@@ -7,6 +8,7 @@ import Comment from '../entities/Comment';
 
 import auth from '../middleware/auth'
 import status from '../middleware/status';
+import { getConnection } from 'typeorm';
 
 const vote = async (req: Request, res: Response) => {
   const {identifier, slug, commentIdentifier, value} = req.body;
@@ -66,7 +68,34 @@ const vote = async (req: Request, res: Response) => {
     return res.status(500).json({ error: 'Internal server error' })
   }
 }
+
+/**
+ * 
+ * @param req 
+ * @param res 
+ * @returns Array of groups in the way that the frontend expects it
+ */
+const topGroups = async (_: Request, res: Response) => {
+  const imageUrl = `COALESCE('${process.env.BASE_URL}/images/' || s."imageUrn", 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y')`;  
+  try {
+    const groups = await getConnection()
+      .createQueryBuilder()
+      .select(`s.title, s.name, ${imageUrl} as "imageUrl", count(p.id) as "postCount"`)
+      .from(Group, 's')
+      .leftJoin(Post, 'p', 's.name = p."subName')
+      .groupBy('s.title, s.name, "imageUrl"')
+      .orderBy(`"postCount"`, 'DESC')
+      .limit(10)
+      .execute()
+
+    return res.json(groups)
+  } catch (err) {
+    return res.status(500).json({ error: 'Internal server error' })
+  }
+}
+
 const router = Router();
 
 router.post('/vote', status, auth, vote);
+router.get('/top-groups', topGroups);
 export default router;
